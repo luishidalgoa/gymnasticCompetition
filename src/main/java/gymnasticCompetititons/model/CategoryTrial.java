c7d50fc5ea0e3d85f6d70762eb0a76fc116a8e40
Isabel <isabelroldancordoba@hotmail.com>
2023-03-07 13:35:52 +0100

    enum

--- src/main/java/gymnasticCompetititons/model/CategoryTrial.java
+++ src/main/java/gymnasticCompetititons/model/CategoryTrial.java
@@ -6,5 +6,20 @@ public enum CategoryTrial {
     ALEVIN,
     INFANTIL,
     JUNIOR,
-    SENIOR
+    SENIOR;
+    
+    /**
+     * Este metodo comprobara que el nombre introducido por el usuario sea igual que alguno de los Enumeradores ya
+     * predefinidos
+     * @param name nombre del tipo introducido por el usuario
+     * @return devuelve el Enum si lo ha encontrado. si no devuelvo Null
+     */
+    public CategoryTrial fromName(String name){
+        for (CategoryTrial e: CategoryTrial.values()){
+            if(e.name().equalsIgnoreCase(name)){
+                return e;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/gymnasticCompetititons/model/KitTrial.java
+++ src/main/java/gymnasticCompetititons/model/KitTrial.java
@@ -5,5 +5,20 @@ public enum KitTrial {
 	MACES,
 	RIBBON,
 	ROPE,
-	HANDSFREE
+	HANDSFREE;
+	
+	/**
+     * Este metodo comprobara que el nombre introducido por el usuario sea igual que alguno de los Enumeradores ya
+     * predefinidos
+     * @param name nombre del tipo introducido por el usuario
+     * @return devuelve el Enum si lo ha encontrado. si no devuelvo Null
+     */
+    public KitTrial fromName(String name){
+        for (KitTrial e: KitTrial.values()){
+            if(e.name().equalsIgnoreCase(name)){
+                return e;
+            }
+        }
+        return null;
+    }
 }
